8524f8ac8a3b8b59f411b691956f9e752140bd95
Supreeta123 <102340222+Supreeta123@users.noreply.github.com>
2022-04-28 12:09:00 +0530

    using postman method

---
+++ src/main/java/com/example/college/Dto/StudentDetailss.java
@@ -0,0 +1,21 @@
+package com.example.college.Dto;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+    @Getter
+    @Setter
+    @EqualsAndHashCode
+    @ToString
+    public class StudentDetailss {
+
+        private String FirstName;
+        private String lastName;
+        private String branch;
+        private String mobileNumber;
+
+    }
+
+
